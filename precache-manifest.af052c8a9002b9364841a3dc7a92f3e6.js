9d7a23d759d50567595494888b6b9fe2fb6ab761
elysee eve <elysee.eve@codeloccol.org>
2020-10-19 15:51:50 +0100

    Updates

---
+++ precache-manifest.af052c8a9002b9364841a3dc7a92f3e6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1673b4749f8777b0a3413be31694b6bb",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "8208be99782972a490d5",
+    "url": "/youtube/static/css/main.32c95d58.chunk.css"
+  },
+  {
+    "revision": "08f21ddc3bd6f5d22ef3",
+    "url": "/youtube/static/js/2.abb9065c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/youtube/static/js/2.abb9065c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8208be99782972a490d5",
+    "url": "/youtube/static/js/main.0abf7e47.chunk.js"
+  },
+  {
+    "revision": "beafb3a44c4ab041b1e3",
+    "url": "/youtube/static/js/runtime-main.cdae53aa.js"
+  },
+  {
+    "revision": "ffb04a4e5580cf17b9d2e549c1894bb9",
+    "url": "/youtube/static/media/Ibrahim Yacouba.ffb04a4e.jpg"
+  },
+  {
+    "revision": "fac507f3f816eed223453387a5b01444",
+    "url": "/youtube/static/media/Mahamadou Issoufou.fac507f3.jpg"
+  },
+  {
+    "revision": "6e20beeff322cbc2b6ce8c038f53835f",
+    "url": "/youtube/static/media/Mahamane Ousmane .6e20beef.jpg"
+  },
+  {
+    "revision": "93bc037e28439f0f32e9c51d8f613d18",
+    "url": "/youtube/static/media/Salou Djibo .93bc037e.jpg"
+  },
+  {
+    "revision": "41d3855445466e6f1eadef14cca1af1b",
+    "url": "/youtube/static/media/Seyni Oumarou.41d38554.jpg"
+  },
+  {
+    "revision": "45db901f391c0a4898b1c62ce33b2710",
+    "url": "/youtube/static/media/jpg.45db901f.jpeg"
+  },
+  {
+    "revision": "011950c48602cfbe1b3ded86668d930b",
+    "url": "/youtube/static/media/mnrd.011950c4.jpeg"
+  },
+  {
+    "revision": "c3738967791722a23c29e241ad7d3e34",
+    "url": "/youtube/static/media/mnsd.c3738967.png"
+  },
+  {
+    "revision": "df813ef26d3c05f233fbc75d7dd526fa",
+    "url": "/youtube/static/media/mpn.df813ef2.png"
+  },
+  {
+    "revision": "3514f54efc60e5531c7870abbcb8728c",
+    "url": "/youtube/static/media/pnds.3514f54e.jpeg"
+  }
+]);
